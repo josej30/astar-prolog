4687e5f89256b6cca0fbfba6ce6daa1b43cc6ef5
jose <jose@vancouver.(none)>
2011-03-08 18:22:55 -0430

    comentado el codigo y terminando de hacer bien la salida del algoritmo

--- astar.pl
+++ astar.pl
@@ -1,26 +1,45 @@
-/* Definicion de A* */
+/* Implementacion de A* */
 
-/* Que necesita?
+/* 
+*
+* astar
 * 
 * -> Nombre del problema a resolver
 * -> Lista de caminos explorados hasta el momento 
 *    con su respectivo costo final
-* <- Costo y estado final
+* -> Lista de nodos visitados
+* <- Costo final y camino recorrido
+*
 */
 
-/* 
-Tengo que revisar si el ultimo camino expandido queda al principio. 
-Esto para no tener que revisar toooodos los caminos y mapearle la funcion objective
-*/
-astar(Problema,[[Costo,Estado|Estados]|Caminos],Visitados,Solucion) :- 
+astar(Problema,[[Costo,Estado|Estados]|Caminos],Visitados,[Costo|Solucion]) :- 
     objective(Problema,Estado),
-    append([Costo],[Estado],Solucion).
+    salida([Costo,Estado|Estados],Solucion).
 astar(Problema,[[Costo,Estado|Estados]|Caminos],Visitados,Solucion) :-
     procesar(Problema,[[Costo,Estado|Estados]|Caminos],Visitados,Posibles),
     renovar(Posibles,[[Costo,Estado|Estados]|Caminos],Visitados,VisitadosNuevos,Nuevos),
     astar(Problema,Nuevos,VisitadosNuevos,Solucion).
 
-/* Revisar que el findall lo este logrando bien */
+salida([],[]).
+salida([Costo,Estado|Estados],[Estado|Solucion]) :-
+    salida(Estados,Solucion).
+
+/* 
+*
+* procesar
+*
+* -> Nombre del problema 
+* -> Lista de caminos abiertos hasta el momento
+* -> Lista de nodos visitados
+* <- Lista con los nuevos posibles estados que se van a expandir.
+*    
+*    Nota: 
+*    Dentro de esa lista están todos los nuevos (posibles) caminos expandidos
+*    sin tomar en cuenta caminos que lleguen a un nodo al que ya se
+*    habia llegado previamente, ya que al ser A*, si llegaste antes
+*    al nodo fue por un camino de menor costo.
+*
+*/
 procesar(Problema,[[Costo,Estado|Estados]|Caminos],Visitados,P) :-
     findall(
 	[CostoEstrella,CostoReal,EstadoNuevo,Costo,Estado|Estados],
@@ -39,28 +58,77 @@ procesar(Problema,[[Costo,Estado|Estados]|Caminos],Visitados,P) :-
     append(Posibles,Posibles2,P).
 procesar(_,_,_,[]).
 
+
+/* 
+*
+* renovar
+*
+* -> Lista de caminos expandidos posibles para escoger
+* -> Lista de caminos que se encuentran abiertos
+* -> Lista con los estados visitados originales
+* <- Lista con los estados visitados luego de escoger el nuevo camino
+* <- Lista con los caminos originales masa el camino escogido para expandir
+*    
+*    Nota: 
+*    Notese que el nuevo estado que se expande siempre se coloca de primero en
+*    la lista resultante. Esto nos asegura que podemos buscar el nodo final 
+*    a salvo siempre en el primer elemento de la lista de caminos resultantes.
+*
+*/
 renovar([[CostoEstrella,CostoReal,EstadoNuevo|R1]|Ns],[[Costo,Estado|Estados]|Caminos],VV,[EstadoN|VV],
         [[CostoN,EstadoN|EstadosN]|[[Costo,Estado|Estados]|Caminos]]) :-
     caminoMinimo([[CostoEstrella,CostoReal,EstadoNuevo|R1]|Ns],CM),
     renovarCaminos([[CostoEstrella,CostoReal,EstadoNuevo|R1]|Ns],CM,[CostoN,EstadoN|EstadosN]).
 
+
+/* 
+*
+* caminoMinimo
+*
+* -> Lista de posibles caminos para ser expandidos
+* <- Costo minimo que se escoge en este paso de A*
+*    
+*    Resumen: 
+*    Basicamente se busca en todos los caminos posibles para
+*    expandir el que tenga el costo menor de g(x); recordando que
+*    g(x) = f(x) + h(x)
+*
+*/
 caminoMinimo([[CostoEstrella,_,_|_]|Ns],CostoMinimo) :-
     caminoMinimo(Ns,CM), CostoMinimo is min(CM,CostoEstrella).
 caminoMinimo([[CostoEstrella,_,_|_]],CostoEstrella).
 
+
+/* 
+*
+* renovarCaminos
+*
+* -> Lista con todos los caminos posibles ya expandidos
+* -> Costo minimo que se escoge en este paso de A*
+* <- Camino de costo minimo que se escoge en este paso de A*
+*   
+*    Resumen: 
+*    Se revisan todos los posibles caminos hasta encontrar el del
+*    costo minimo.
+*
+*/
 renovarCaminos([],_,[]).
 renovarCaminos([[CM,CostoReal,EstadoNuevo|R1]|Ns],CM,[CostoReal,EstadoNuevo|R1]).
 renovarCaminos([[CostoEstrella,CostoReal,EstadoNuevo|R1]|Ns],CM,RestoCaminos) :- 
     CostoEstrella \== CM, renovarCaminos(Ns,CM,RestoCaminos).
 
+/*---------- Hasta aqui es A* ------------*/
 
-/* Para resolver problemas con esta implementacion de A* */
+
+/* Predicado para resolver comodamente los problemas en A* */
 
 resolver(Problema,Solucion) :-
     inicial(Problema,Estado),
     astar(Problema,[[0,Estado]],[Estado],Solucion).
 
+/************************/
 /* Ejemplo de Wikipedia */
+/************************/
 
 /* Represento un estado como (nodo_actual,[nodos_posibles]) */
 
